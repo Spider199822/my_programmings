da1e828dcc0e4f7f18abfe9ad0f9dde4663fbc3c
AmirhosseinSharif <74930759+Spider199822@users.noreply.github.com>
2022-09-06 12:32:50 +0430

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Antegral_polynomial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Polynomial p1 = new Polynomial(4);
+            p1.Integral(p1);
+            p1.print();
+            Console.ReadKey();
+        }
+    }
+}
